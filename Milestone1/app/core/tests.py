24538cc648f3b57cabd1e85eed21b4b95930de37
ryan.manny <pie4prez@gmail.com>
2019-10-28 21:52:51 -0700

    Stub in everything we need to test

--- Milestone1/app/core/tests.py
+++ Milestone1/app/core/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestRoommateModel(TestCase):
+    def test_find_compatible_roommates(self):
+        pass
+
+    def test_find_compatible_apartments(self):
+        pass
+
+
+class TestApartmentModel(TestCase):
+    def test_find_compatible_tenants(self):
+        pass
+
+
+class TestViews(TestCase):
+    def test_home(self):
+        pass
+
+    def test_tenant_home(self):
+        pass
+
+    def test_landlord_home(self):
+        pass
+
+    def test_roommate_form(self):
+        pass
+
+    def test_roommate_list(self):
+        pass
+
+    def test_good_roommate_list(self):
+        pass
+
+    def test_roommate_detail(self):
+        pass
+
+    # Does not need to be tested! Has no special functionality
+    # def test_apartment_form(pass):
+    #     pass
+
+    def test_apartment_list(self):
+        pass
+
+    def test_apartment_detail(self):
+        pass
